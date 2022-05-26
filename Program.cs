865c2fcc80c7ee7bdb69acbc9a348902fd88759c
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-26 14:37:24 -0700

    updated messages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Everyone!");
+Console.WriteLine($"Hi, Enter your name below:");
+var teacherName = Console .ReadLine();
+
+Console.WriteLine($"Hello, {teacherName}!  How are you today?");Console.WriteLine("Hello, Everyone!");
