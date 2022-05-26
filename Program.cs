e3a056a3a5746793ec10f444aefc067f8f041a45
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-26 14:46:00 -0700

    new questions

--- Program.cs
+++ Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine($"Hi, Enter your name below:");
 var teacherName = Console .ReadLine();
 
 Console.WriteLine($"Hello, {teacherName}!  How are you today?");
+var teacherName = Console .ReadLine();
+
+Console.WriteLine($"Glad to hear it {teacherName}!");
